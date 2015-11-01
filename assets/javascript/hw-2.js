28874c357f87c43fa3311d2ccdd7b66394b45a1a
krysradz <krys.radz@gmail.com>
2015-11-01 18:43:42 -0500

    nested files correctly

---
+++ assets/javascript/hw-2.js
@@ -0,0 +1,13 @@
+// 1. Write a program that checks if a variable is less than 10.
+// If it is, alert the user that their variable is less than 10.
+// If it is not, let the user know what the variable was and that it was greater than 10.
+// 2.  Declare a function that takes a name as an argument and tells the user what name
+// they've entered, try invoking it after it has been declared
+// 3. Declare a function that takes no arguments but prints something to the console,
+// try invoking it after it has been declared
+// 4.  Create a `for` loop that "sings" the classic song "99 Bottles of Root Beer on the Wall".
+// The code it outputs to the console should look similar to this:
+// "99 bottles of root beer on the wall, 99 bottles of root beer...take one down,
+// pass it around 98 bottles of root beer on the wall, 98 bottles of root beer on the wall,
+// 98 bottles of root beer...take one down,
+// pass it around 97 bottles of root beer on the wall," etc., all the way to 0 bottles.
